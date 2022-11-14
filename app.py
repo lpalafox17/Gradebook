e2b17f7f56bc9b3f7e1ad53f894a11676b02d1b6
Salvador Avelar-Lopez <100443266+salvadoravelar@users.noreply.github.com>
2022-11-13 17:57:52 -0800

    updated flask-admin

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-#! /usr/bin/python3
-
 from flask import Flask, abort, render_template, url_for, request, redirect, jsonify, json
 from flask_sqlalchemy import SQLAlchemy
-from flask_admin import Admin
+from flask_admin import Admin, AdminIndexView
 from flask_admin.contrib.sqla import ModelView
 from flask import jsonify
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
@@ -11,82 +9,106 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
 
-
 app = Flask(__name__)
+db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.sqlite'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SECRET_KEY'] = 'secretkey'
-db = SQLAlchemy(app)
-
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = "login"
+login_manager.login_view = 'login'
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+# admin = Admin(app, index_view=MyAdminModelView)
+admin = Admin(app)
 
-class User(db.Model): #-------------------------------------------------------------
-    __tablename__ = 'User'
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(100), unique = True, nullable = False)
-    password = db.Column(db.String(100), unique = True, nullable = False)
+
+
+class User(db.Model, UserMixin): #-------------------------------------------------------------
+    id = db.Column('id', db.Integer, primary_key = True)
+    username = db.Column('username', db.String(100))
+    password = db.Column('password', db.String(100))
+    def __repr__(self):
+        return '<User %r>' % (self.username)
 
 class Student(db.Model): #-------------------------------------------------------------
     __tablename__ = 'Student'
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(100), unique = True, nullable = False)
-    userID = db.Column('userID', db.ForeignKey(User.id), nullable = False)
+    id = db.Column('id', db.Integer, primary_key = True)
+    name = db.Column('name', db.String(100))
+    user_id = db.Column('user_id', db.ForeignKey('user.id'), nullable = False)
     user = db.relationship('User', backref=db.backref('Student', uselist=False))
+    def __repr__(self):
+        return '<Student %r>' % (self.user_id)
+
+class StudentView(ModelView):
+    column_list = ['user.username', 'name']
 
 class Teacher(db.Model): #-------------------------------------------------------------
-    __tablename__ = 'Teacher'
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(100), unique = True, nullable = False)
-    userID = db.Column('userID', db.ForeignKey(User.id), nullable = False)
+    id = db.Column('id', db.Integer, primary_key = True)
+    name = db.Column('name', db.String(100), unique = True)
+    user_id = db.Column('user_id', db.ForeignKey('user.id'), nullable = False)
     user = db.relationship('User', backref=db.backref('Teacher', uselist=False))
+    def __repr__(self):
+        return '<Teacher %r>' % (self.user_id)
+
+class TeacherView(ModelView):
+    column_list = ['user.username', 'name']
 
 class Courses(db.Model): #-------------------------------------------------------------
     __tablename__ = 'Courses'
-    id = db.Column(db.Integer, primary_key = True, unique = True)
-    courseName = db.Column(db.String(100), unique = True, nullable = False)
-    teacherID = db.Column('teacherID', db.ForeignKey(Teacher.id), nullable = False)
-    enrollNum = db.Column(db.Integer, unique = True, nullable = False)
-    capacity = db.Column(db.Integer, unique = True, nullable = False)
-    time = db.Column(db.String(100), unique = False, nullable = False)
-    teacher = db.relationship('Teacher', backref = ('Courses'))
+    id = db.Column('id', db.Integer, primary_key = True)
+    courseName = db.Column('courseName', db.String(100))
+    teacher_id = db.Column('teacher_id', db.ForeignKey('teacher.id'), nullable = False)
+    enrollNum = db.Column('enrollNum', db.Integer)
+    capacity = db.Column('capacity', db.Integer)
+    time = db.Column('time', db.String(100))
+    teacher = db.relationship('Teacher', backref=db.backref('Courses'))
+    def __repr__(self):
+        return '<Courses %r>' % (self.id)
+
+class CoursesView(ModelView):
+    column_list = ['courseName', 'teacher.name', 'enrollNum', 'capacity', 'time']
 
 class Enrollment(db.Model): #-------------------------------------------------------------
     ____tablename__ = 'Enrollment'
-    id = db.Column(db.Integer, primary_key = True, unique = True)
-    courseID = db.Column('courseID', db.ForeignKey(Courses.id))
-    studentID = db.Column('studentID', db.ForeignKey(Student.id))
-    grade = db.Column(db.Integer, unique = False)
-    course = db.relationship('Courses', backref = ('Enrollment'))
-    student = db.relationship('Student', backref = ('Enrollment'))
+    id = db.Column('id', db.Integer)
+    course_id = db.Column('course_id', db.ForeignKey('Courses.id'), primary_key = True)
+    student_id = db.Column('student_id', db.ForeignKey('Student.id'), primary_key = True)
+    grade = db.Column('grade', db.Integer)
+    courses = db.relationship('Courses', backref=db.backref('Enrollment'))
+    student = db.relationship('Student', backref=db.backref('Enrollment'))
+    def __repr__(self):
+        return '<Enrollment %r>' % (self.student_id)
+
+class EnrollmentView(ModelView):
+    column_labels = {'student.name': 'Student'}
+    column_list = ['student.name', 'course.courseName', 'grade']
 
 
 
-class RegistrationForm(FlaskForm):
-    username = StringField(validators=[InputRequired(), Length(min=4, max=25)], render_kw={"placeholder": "Username"})
-    password = PasswordField(validators=[InputRequired(), Length(min=4, max=25)], render_kw={"placeholder": "Password"})
-    submit = SubmitField("Register")
-    def validate_username(self, username):
-        existing_user_username = User.query.filter_by(username=username.data).first()
-        if existing_user_username:
-            raise ValidationError("That username already exists. Please choose a different one.")
+#-- Admin Model View for Tables ----------------------------------------------
+admin.add_view(ModelView(User, db.session))
+admin.add_view(StudentView(Student, db.session))
+admin.add_view(TeacherView(Teacher, db.session))
+admin.add_view(CoursesView(Courses, db.session))
+admin.add_view(EnrollmentView(Enrollment, db.session))
 
+
+#--Flask Login-in Form-------------------------------------------------------
 class LoginForm(FlaskForm):
-    username = StringField(validators=[InputRequired(), Length(min=4, max=25)], render_kw={"placeholder": "Username"})
-    password = PasswordField(validators=[InputRequired(), Length(min=4, max=25)], render_kw={"placeholder": "Password"})
-    submit = SubmitField("Login")
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Password"})
+    submit = SubmitField('Login')
 
 
-@app.route('/')
-def index():
+
+@app.route('/') #--------------------------------------------- HOME ROUTE --
+def home():
     # return "Hello, This is the main page <h1>HELLO</h1>"
     return render_template('home.html')
 
@@ -97,19 +119,28 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user:
-            if bcrypt.check_password_hash(user.password, form.password.data):
-                login_user(user)
-                return redirect(url_for('dashboard'))
+            # if bcrypt.check_password_hash(user.password, form.password.data):
+            if user.password == form.password.data:
+                student = db.session.query(User).join(Student).filter(User.id == user.id).all()
+                teacher = db.session.query(User).join(Teacher).filter(User.id == user.id).all()
+                if student:
+                    login_user(user)
+                    # return redirect(url_for('dashboard'))
+                    return redirect('/student')
+                elif teacher:
+                    login_user(user)
+                    return redirect('/teacher')
+                else:
+                    login_user(user)
+                    return redirect(url_for('admin'))
     return render_template('login.html', form=form)
 
-
-    # return "Hello, This is the main page <h1>HELLO</h1>"
-    # return render_template('login.html', form = form)
 
 @app.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def dashboard():
     return render_template('dashboard.html')
+
 
 @app.route('/logout', methods=['GET', 'POST'])
 @login_required
@@ -118,18 +149,23 @@ def logout():
     return redirect(url_for('login'))
 
 
+@app.route('/student', methods=['GET', 'POST'])
+def student():
+    # user = current_user.id
+    # studentLogged = Student.query.filter_by(user_id=user).first()
+    # s = studentLogged.id
+    return render_template('student.html')
 
-# register flask-admin with app
-admin = Admin(app)
 
-# create a view of flask-admin according to the database
-admin.add_view(ModelView(User, db.session))
-admin.add_view(ModelView(Teacher, db.session))
-admin.add_view(ModelView(Student, db.session))
-admin.add_view(ModelView(Courses, db.session))
-admin.add_view(ModelView(Enrollment, db.session))
+@app.route('/teacher', methods=['GET', 'POST'])
+def teacher():
+    # user = current_user.id
+    # studentLogged = Student.query.filter_by(user_id=user).first()
+    # s = studentLogged.id
+    return render_template('teacher.html')
+
 
 if __name__ == "__main__":
-   #db.drop_all()
-  #  db.create_all()
+    # db.drop_all()
+    # db.create_all()
     app.run(debug = True)
